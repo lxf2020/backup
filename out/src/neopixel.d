e68dbb160b4b799ec0b37cb0abaef46e89c58df5
lxf2020 <li_xuef@163.com>
2020-02-15 22:54:34 +0800

    update

---
+++ out/src/neopixel.d
@@ -0,0 +1,12 @@
+out/src/neopixel.o: src/neopixel.c out/autoconf.h out/board/gpio.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ out/board/irq.h /usr/lib/avr/include/avr/interrupt.h \
+ /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
+ /usr/lib/avr/include/inttypes.h /usr/lib/avr/include/avr/iom2560.h \
+ /usr/lib/avr/include/avr/iomxx0_1.h /usr/lib/avr/include/avr/portpins.h \
+ /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
+ /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
+ src/compiler.h out/board-generic/board/misc.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /usr/lib/gcc/avr/5.4.0/include/stddef.h src/basecmd.h src/command.h \
+ src/ctr.h src/compiler.h
